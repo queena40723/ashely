f25e25c31dd4192419f067bcfcabf430fae72827
ashleylai58 <ashleylai58@gmail.com>
2021-12-26 14:12:37 +0800

    add sum quiz

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+// sum.js
+
+function sum(n) {
+  // TODO:
+  // result = 1 + 2 + 3 + .... + n
+  return result;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(5)); // 15
+console.log(sum(10)); // 55
