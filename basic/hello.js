231ea47cd2014df468c48e9f5d25322e39deb03e
ashleylai58 <ashleylai58@gmail.com>
2022-01-08 14:27:15 +0800

    add callback1

--- basic/hello.js
+++ basic/hello.js
@@ -1,9 +1,18 @@
-let name = "小賴";
-
-console.log(`Hello Node from ${name}`);
-
 console.log("start");
+
 setTimeout(function () {
-  console.log("Timeout");
-}, 1000);
+  console.log("Timeout1");
+  // 要花很久時間的事
+  for (let i = 0; i < 100000; i++) {
+    let result = 0;
+    for (let j = 0; j < 100000; j++) {
+      result += j;
+    }
+  }
+}, 0);
+
+setTimeout(function () {
+  console.log("Timeout2");
+}, 0);
+
 console.log("end");
