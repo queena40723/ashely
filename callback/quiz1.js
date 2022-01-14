fd5a18817157f80cc4cf1895b67d4e74a5ab7d83
ashleylai58 <ashleylai58@gmail.com>
2022-01-14 22:14:36 +0800

    fix quiz answer

--- callback/quiz1.js
+++ callback/quiz1.js
@@ -1,18 +1,16 @@
-// 沒有 await => 41235
-// 有 await => 41523
 async function test() {
   console.log(1);
-
-  await new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log(2);
       resolve();
     }, 0);
   });
-
   console.log(3);
 }
-
 console.log(4);
 test();
 console.log(5);
+
+// 沒有 await => 41352
+// 有 await => 41523
