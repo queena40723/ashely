ed52ff6bd03eb385c73dee7c9f6387226a8fe051
ashleylai58 <ashleylai58@gmail.com>
2022-01-15 15:48:25 +0800

    add dotenv

--- crawler/crawler5.js
+++ crawler/crawler5.js
@@ -2,14 +2,15 @@ const axios = require("axios");
 const { readFile } = require("fs/promises");
 const moment = require("moment");
 const mysql = require("mysql2");
+require("dotenv").config();
 
 (async () => {
   let connection = mysql.createConnection({
-    host: "localhost", // 127.0.0.1
-    port: 3306,
-    user: "????",
-    password: "???",
-    database: "stock_mfee22",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   try {
