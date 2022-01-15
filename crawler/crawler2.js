0963702e3037f8f124c9b7ad2a3cac4f30a80b0b
ashleylai58 <ashleylai58@gmail.com>
2022-01-15 11:41:33 +0800

    add await crawler

---
+++ crawler/crawler2.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+(async () => {
+  try {
+    // 根據變數去抓取資料
+    let stockNo = 2330;
+    let queryDate = "20220115";
+
+    // let response = await axios.get(
+    //   `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${stockNo}`
+    // );
+
+    let response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        // 這裡可以放一些設定
+        // params: 放 query string 的參數
+        params: {
+          response: "json",
+          date: queryDate,
+          stockNo,
+        },
+      }
+    );
+
+    console.log(response.data);
+  } catch (e) {
+    console.error(e);
+  }
+})();
